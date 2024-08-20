88b49ad9e677b441bb74144da2571ae0ea044906
nate5461 <charliebissett906@gmail.com>
2024-08-20 13:02:19 -0400

    save game changes to database

--- datab.py
+++ datab.py
@@ -4,7 +4,8 @@ def create_tables():
     conn = sqlite3.connect('oil_data.sqlite')
     c = conn.cursor()
     
-    #c.execute('''CREATE TABLE IF NOT EXISTS spreadTransactions (transaction_id INTEGER PRIMARY KEY, transaction_date TEXT, qty INTEGER, frontMonth TEXT, backMonth TEXT, type TEXT, limit_price REAL, status TEXT, purchase_date TEXT)''')
+    c.execute('''DROP TABLE IF EXISTS spreadTransactions''')
+    c.execute('''CREATE TABLE IF NOT EXISTS SavedGames (Trans_id INTEGER PRIMARY KEY, trans_date TEXT, contract_date TEXT, qty INTEGER, limit_price REAL, status TEXT, purchase_date TEXT, type TEXT, purchase_price REAL, trans_price REAL, close_qty INTEGER, close_limit REAL, game INTEGER)''')
     #c.execute('''ALTER TABLE wallet ADD COLUMN excess INTEGER;''')
     
     conn.commit()
